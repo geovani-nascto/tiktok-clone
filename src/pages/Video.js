d1c1da3af32555d0a5af64603d8cf1a8dcc1463b
geovani-nascto <104122527+geovani-nascto@users.noreply.github.com>
2023-06-21 14:26:32 -0300

    att v2 - frontend and video component
    Construção do container e do componente vídeo

---
+++ src/pages/Video.js
@@ -0,0 +1,35 @@
+import React, { useRef, useState } from 'react'
+import './video.css'
+
+function Video() {
+
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
+
+    function handdleStart(){
+        if(play){
+            videoRef.current.pause()
+            setPlay(false)
+        } else{
+            videoRef.current.play()
+            setPlay(true)
+        }
+    }
+
+    return (
+        <div className="video">
+            <video
+            ref={videoRef}
+            className="video__player"
+            onClick={handdleStart}
+            src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+            controls
+            loop
+            >
+
+            </video>
+        </div>
+    )
+}
+
+export default Video
